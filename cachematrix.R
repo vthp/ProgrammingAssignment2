6184c14752e7c018eb1346e056dabe8caf6e7348
unknown <vphillips@9MB8TZ1.turn.corp>
2015-03-16 23:16:13 +0000

    File was edited with new code and resubmit as instructed.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## I'm using the example to try and set
+## the value of a matrix.
+## Essentially we are using the solve instead
+## of using the mean.
+## We then want to get the inverse of the
+## given matrix either using the cache if
+## it exists there, or computing it.
 
-## Write a short comment describing this function
+## This first function initialises m to
+## NULL. We then set the function to solve
+## and cache the value. set, get, setinverse
+## and getinverse are then stored as a vector.
+## The environment in which these caches 
+## were defined can be called to use the
+## passed values.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+m<-NULL
+set<-function(y){
+  x<<-y
+  m<<-NULL
+}
+get<-function() x
+setinverse<-function(solve) m<<-solve
+getinverse<-function() m
+list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## Calls elements of the vector list from
+## makeCacheMatrix and can enter the original
+## defining environment.
+## If result is returned from the cache
+## then this is noted in a messsage.
+## Else, the message is returned.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {
+  m<-x$getinverse()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  data<-x$get()
+  m<-solve(data, ...)
+  x$setinverse(m)
+  m
 }
